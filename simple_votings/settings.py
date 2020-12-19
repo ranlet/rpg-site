027bb9d0380b020f59dcb8e9a25670f973774611
Andrew Smirnov <smirnov@informatics.ru>
2020-12-19 16:38:11 +0300

    Remove secret key

--- simple_votings/settings.py
+++ simple_votings/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Далее полученное значение подставляется в соответствующую переменную
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Indsert secret key here and uncomment this variable'
+# SECRET_KEY = 'Insert secret key here and uncomment this variable'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
